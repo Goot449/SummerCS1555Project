97a66a967d7ba8f3070d821c190395d2a501a772
connor <cjk68@pitt.edu>
2016-07-11 17:42:34 -0400

    Table changes
    - changed some attribute names to be more readable
    - added some table constraints

--- database.sql
+++ database.sql
@@ -16,12 +16,14 @@ purge recyclebin;
 --Assuming that all users data is necessary (name, email, etc.)
 CREATE TABLE users
 (
-    usersID NUMBER(10) NOT NULL,
+    userID NUMBER(10) NOT NULL,
     name VARCHAR2(50) NOT NULL,
     email VARCHAR2(50) NOT NULL,
     dob DATE NOT NULL,
-    lastLogin DATE,--time needed?
-    CONSTRAINT users_pk PRIMARY KEY (usersID)
+    lastLogin TIMESTAMP,
+    CONSTRAINT users_pk PRIMARY KEY (userID),
+    CONSTRAINT users_email UNIQUE (email) 
+    --an email can only be registered under one user
 );
 --Allows new users to be added without needing to know the next available usersID
 --to use: call seq_userID.nextval
@@ -29,21 +31,21 @@ CREATE SEQUENCE seq_usersID START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE friends
 (
-    usersID1 NUMBER(10) NOT NULL,
-    usersID2 NUMBER(10) NOT NULL,
+    userID1 NUMBER(10) NOT NULL,
+    userID2 NUMBER(10) NOT NULL,
     dateEstablished DATE NOT NULL,
-    CONSTRAINT friends_pk PRIMARY KEY (usersID1, usersID2),
-    CONSTRAINT friends_fk1 FOREIGN KEY (usersID1) REFERENCES users(usersID),
-    CONSTRAINT friends_fk2 FOREIGN KEY (usersID2) REFERENCES users(usersID)
+    CONSTRAINT friends_pk PRIMARY KEY (userID1, userID2),
+    CONSTRAINT friends_fk1 FOREIGN KEY (userID1) REFERENCES users(userID),
+    CONSTRAINT friends_fk2 FOREIGN KEY (userID2) REFERENCES users(userID)
 );
 
 CREATE TABLE pendingFriends
 (
-    fromID NUMBER(10) NOT NULL,
+    requestID NUMBER(10) NOT NULL,
     toID NUMBER(10) NOT NULL,
-    CONSTRAINT pendingFriends_pk PRIMARY KEY (fromID, toID),
-    CONSTRAINT pendingFriends_fk1 FOREIGN KEY (fromID) REFERENCES users(usersID),
-    CONSTRAINT pendingFriends_fk2 FOREIGN KEY (toID) REFERENCES users(usersID)
+    CONSTRAINT pendingFriends_pk PRIMARY KEY (requestID, toID),
+    CONSTRAINT pendingFriends_fk1 FOREIGN KEY (requestID) REFERENCES users(userID),
+    CONSTRAINT pendingFriends_fk2 FOREIGN KEY (toID) REFERENCES users(userID)
 );
 
 --assuming group description is not longer than 255 characters
@@ -62,34 +64,36 @@ CREATE TABLE groupMembership
 (
     membershipID NUMBER(10) NOT NULL,
     groupID NUMBER(10) NOT NULL,
-    usersID NUMBER(10) NOT NULL,
+    userID NUMBER(10) NOT NULL,
     CONSTRAINT groupMembership_pk PRIMARY KEY (membershipID),
     CONSTRAINT groupMembership_fk1 FOREIGN KEY (groupID) REFERENCES groupInfo(groupID),
-    CONSTRAINT groupMembership_fk2 FOREIGN KEY (usersID) REFERENCES users(usersID)
+    CONSTRAINT groupMembership_fk2 FOREIGN KEY (userID) REFERENCES users(userID)
 );
 --trigger needed to ensure groupInfo.memberLimit is enforeced
 
---if tousersID is not null, message will be sent there. Otherwise, check toGroupID and send to all group members
+--if recipientID is not null, message will be sent there. Otherwise, check toGroupID and send to all group members
 --add respective rows to groupMessageRecipients
 CREATE TABLE messages
 (
     msgID NUMBER(10) NOT NULL,
-    fromID NUMBER(10) NOT NULL,
-    tousersID NUMBER(10) DEFAULT NULL,
+    senderID NUMBER(10) NOT NULL,
+    recipientID NUMBER(10) DEFAULT NULL,
     toGroupID NUMBER(10) DEFAULT NULL, 
     subject VARCHAR2(50) NOT NULL,
     message VARCHAR2(100) NOT NULL,
     dateSent DATE NOT NULL,
     CONSTRAINT msg_pk PRIMARY KEY (msgID),
-    CONSTRAINT msg_fk1 FOREIGN KEY (fromID) REFERENCES users(usersID)
+    CONSTRAINT msg_fk1 FOREIGN KEY (senderID) REFERENCES users(userID),
+    CONSTRAINT msg_check CHECK (recipientID NOT NULL OR toGroupID NOT NULL)
+    --check to make sure there is a recipient or group to receive message
 );
 CREATE SEQUENCE seq_msgID START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE groupMessageRecipients
 (
     msgID NUMBER(10) NOT NULL,
-    tousersID NUMBER(10) NOT NULL,
-    CONSTRAINT groupMessageRecipients_pk PRIMARY KEY (msgID, tousersID),
+    recipientID NUMBER(10) NOT NULL,
+    CONSTRAINT groupMessageRecipients_pk PRIMARY KEY (msgID, recipientID),
     CONSTRAINT groupMessageRecipients_fk1 FOREIGN KEY (msgID) REFERENCES messages(msgID),
-    CONSTRAINT groupMessageRecipients_fk2 FOREIGN KEY (tousersID) REFERENCES users(usersID)
+    CONSTRAINT groupMessageRecipients_fk2 FOREIGN KEY (recipientID) REFERENCES users(userID)
 );
