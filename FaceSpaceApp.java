392b77aecc7997b6036c62c552a3bf81e6c77f8b
njs55 <njs55@pitt.edu>
2016-07-21 15:57:01 -0400

    comments added and spacing adjusted

--- FaceSpaceApp.java
+++ FaceSpaceApp.java
@@ -1,16 +1,14 @@
-import java.sql.*;  //import the file containing definitions for the parts
+import java.sql.*;
 import java.text.ParseException;
 import java.util.Scanner;
-
 
 public class FaceSpaceApp {
     private static Connection connection; //used to hold the jdbc connection to the DB
     private Statement statement; //used to create an instance of the connection
-    private PreparedStatement prepStatement; //used to create a prepared statement, that will be later reused
-    private ResultSet resultSet; //used to hold the result of your query (if one
-    // exists)
+    private PreparedStatement prepStatement; //used to create a prepared statement
+    private ResultSet resultSet; //used to hold the result of your query
     private String query;  //this will hold the query we are using
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in); //used to read user input
 
     public FaceSpaceApp(){
         System.out.println("in FaceSpaceApp method");
@@ -18,13 +16,8 @@ public class FaceSpaceApp {
 
     }
 
-
     public static void main(String args[]) throws SQLException {
-        /* Making a connection to a DB causes certain exceptions.  In order to handle
-         these, you either put the DB stuff in a try block or have your function
-         throw the Exceptions and handle them later.  For this demo I will use the
-         try blocks */
-
+        //User is prompted for Oracle Username and Password
         String username, password;
         System.out.print("Enter Oracle Username: ");//This is your username in oracle
         username = scanner.next();
@@ -34,27 +27,21 @@ public class FaceSpaceApp {
         try{
             // Register the oracle driver.
             DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
-
-            //This is the location of the database.  This is the database in oracle
-            //provided to the class
+            //This is the location of the database
             String url = "jdbc:oracle:thin:@class3.cs.pitt.edu:1521:dbclass";
-            System.out.println("trying to connect");
             //create a connection to DB on class3.cs.pitt.edu
             connection = DriverManager.getConnection(url, username, password);
-            FaceSpaceApp fsa = new FaceSpaceApp();
-            System.out.println("connected");
 
+            FaceSpaceApp fsa = new FaceSpaceApp();
         }
         catch(Exception Ex)  {
             System.out.println("Error connecting to database.  Machine Error: " + Ex.toString());
         }
         finally{
             /*
-             * NOTE: the connection should be created once and used through out the whole project;
-             * Is very expensive to open a connection therefore you should not close it after every operation on database
+             * NOTE: the connection should be created once and used through out the whole project
              */
             connection.close();
         }
     }
-
 }
